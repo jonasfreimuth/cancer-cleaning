3e1e3499f182e4a57b74ddb3262e1d9e6bc15963
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-10-18 16:18:44 +0200

    Add guix reproducibility info
      I.e. manifest and channel descriptions.

---
+++ channels.scm
@@ -0,0 +1,11 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "9e4632081ff31bf0d1715edd66f514614c6dc4bb")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
