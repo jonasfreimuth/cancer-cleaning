f3e5489fe3a5b6fb0427cb433f42517703d177c5
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-10-24 11:47:47 +0200

    Format dedupe_sigmut_mat.R fun file

--- functions/dedupe_sigmut_mat.R
+++ functions/dedupe_sigmut_mat.R
@@ -15,23 +15,23 @@ dedupe_sigmut_mat <- function(sigmut_mat, var_sep = "_") {
   #   * A list of character vectors, giving names of variants which have equal
   #     cols.
   variant_names <- colnames(sigmut_mat)
-  
+
   is_dupe <- duplicated(sigmut_mat, MARGIN = 2)
-  
+
   if (any(is_dupe)) {
     dupe_variants <- variant_names[is_dupe]
-    
+
     # coerce back to dataframe for easier processing
     sigmut_mat_df <- as.data.frame(sigmut_mat)
-    
+
     # find out of which variant a dupe variant is a dupe of, generate groups
     # of variants which are duplicates of each other
     dupe_group_list <- list()
-    
+
     for (dupe_var in dupe_variants) {
       if (!dupe_var %in% unique(unlist(dupe_group_list))) {
         dupe_var_col <- sigmut_mat_df[[dupe_var]]
-        
+
         dupe_group_logi <- apply(
           sigmut_mat,
           2,
@@ -39,25 +39,25 @@ dedupe_sigmut_mat <- function(sigmut_mat, var_sep = "_") {
             all(col == dupe_col)
           }, dupe_var_col
         )
-        
+
         dupe_group_vec <- variant_names[dupe_group_logi]
-        
+
         dupe_group_list[[dupe_group_vec[1]]] <- dupe_group_vec
       }
     }
-    
+
     # concat dupe groups to form a new composite name for the now unique col
     dupe_group_names <- lapply(dupe_group_list, paste, collapse = var_sep) %>%
       unlist()
-    
+
     # juggle names to get a named vector with names and values flipped
     # needed by dplyr::rename()
     old_names <- names(dupe_group_names)
     new_names <- dupe_group_names
-    
+
     dupe_group_names <- old_names %>%
       set_names(new_names)
-    
+
     # generate deduped signature matrix
     # is a col was duplicated this contains only the first col of each dupe group
     dedupe_res <- sigmut_mat_df[, !is_dupe] %>%
@@ -67,6 +67,6 @@ dedupe_sigmut_mat <- function(sigmut_mat, var_sep = "_") {
     dedupe_res <- sigmut_mat %>%
       as.data.frame()
   }
-  
+
   return(dedupe_res)
 }
