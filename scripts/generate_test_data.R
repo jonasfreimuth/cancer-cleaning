631da66b8ca74c6f06aa20a0898b5777b563d347
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-11-08 13:25:31 +0100

    Add script for generating test data
      Outsourcing of downsampling of the count matrix should allow for
      testing benchmark script without so much loading time.

---
+++ scripts/generate_test_data.R
@@ -0,0 +1,37 @@
+library("data.table")
+library("here")
+
+here::i_am("scripts/generate_test_data.R")
+
+source("functions/benchmark_functions.R")
+
+seed <- 123
+
+set.seed(seed)
+
+source_data_dir <- here("datasets/Wu_etal_2021_BRCA_scRNASeq/")
+destin_data_dir <- here("datasets/Wu_etal_downsampled_test")
+
+data <- load_experiment(
+  count_mat_file = here(source_data_dir, "count_matrix_sparse.mtx"),
+  rowname_file = here(source_data_dir, "count_matrix_genes.tsv"),
+  colname_file = here(source_data_dir, "count_matrix_barcodes.tsv"),
+  meta_file = here(source_data_dir, "metadata.csv"),
+  testing = TRUE
+)
+
+dir.create(destin_data_dir, showWarnings = FALSE, recursive = TRUE)
+
+writeMM(data$count_mat, here(destin_data_dir, "count_matrix_sparse.mtx"))
+
+writeLines(
+  rownames(data$count_mat),
+  here(destin_data_dir, "count_matrix_genes.tsv")
+)
+
+writeLines(
+  colnames(data$count_mat),
+  here(destin_data_dir, "count_matrix_barcodes.tsv")
+)
+
+fwrite(data$meta, here(destin_data_dir, "metadata.csv"))
