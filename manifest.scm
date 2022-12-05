88f1b8739e55aaad40edf270962fee1a46c10a8d
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-05 12:54:56 +0100

    Combine manifest.scm & channels.scm

--- manifest.scm
+++ manifest.scm
@@ -1,10 +1,42 @@
-;; This "manifest" file can be passed to 'guix package -m' to reproduce
-;; the content of your profile.  This is "symbolic": it only specifies
-;; package names.  To reproduce the exact same profile, you also need to
-;; capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
+;;; Run the following command to enter a development environment:
+;;;
+;;;  $ guix shell -m manifest.scm
+;;;
+;;; When the shell variable USE_GUIX_INFERIOR is set (to any value), a
+;;; specific set of Guix channels will be used to build the
+;;; environment.
 
-(specifications->manifest
+(use-modules
+ (guix profiles)
+ (guix channels)
+ (guix inferior)
+ (ice-9 match))
+
+(define channels
+   (list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "9e4632081ff31bf0d1715edd66f514614c6dc4bb")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+
+(define (lookup name)
+  (specification->package name))
+
+(define (lookup-inferior name)
+  (define inferior
+    (inferior-for-channels channels))
+  (match (lookup-inferior-packages inferior name)
+    ((first . rest) first)
+    (_ (error
+        (format #false "Could not find package `~a'.~%" name)))))
+
+(define %packages
   (list "r-glmgampoi"
         "r-guix-install"
         "libxml2"
@@ -57,3 +89,8 @@
         "r"
         "glibc-locales"
         "nss-certs"))
+
+(packages->manifest
+ (let ((how (if (getenv "USE_GUIX_INFERIOR")
+                lookup-inferior lookup)))
+   (map how (append %packages))))
