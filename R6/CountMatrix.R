fd2f10d6a91178fa473afced4ead787490ec85c8
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-20 15:53:39 +0100

    Add preliminary CountMatrix class

---
+++ R6/CountMatrix.R
@@ -0,0 +1,46 @@
+library("data.table")
+library("magrittr")
+library("dplyr")
+library("R6")
+
+CountMatrix <- R6Class(
+  "CountMatrix",
+  public = list(
+    matrix = NULL,
+    # TODO Ensure meta is sorted by matrix order of cells
+    meta = NULL,
+    initialize = function(matrix, meta) {
+      self$matrix <- matrix
+      self$meta <- meta
+    },
+  ),
+  active = list(
+    cells = function() {
+      colnames(self$matrix)
+    },
+    celltypes = function() {
+      cell_colname <- "cell"
+      celltype_colname <- "celltype_major"
+
+      celltype_map <- self$meta %>%
+        extract(c(cell_colname, celltype_colname))
+
+      dataframe(cells = self$cells) %>%
+        left_join(celltype_map, by = c("cells", cell_colname)) %>%
+        extract2(celltype_colname)
+    },
+    transcripts = function() {
+      rownames(self$matrix)
+    },
+    n_cells = function() {
+      self$cells %>%
+        # NOTE Cells should always be unique, so there is no safeguard here.
+        length()
+    },
+    n_celltypes = function() {
+      self$celltypes %>%
+        unique() %>%
+        length()
+    }
+  )
+)
