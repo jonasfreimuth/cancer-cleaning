cbf22cece0351c8849bddb90a68a74ba624b8e26
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-22 14:59:56 +0100

    Switch Psedudobulk to use composite CountMatrix
      The plan is that everything carries a reference to the original
      count_matrix object created by ScRnaExperiment and just subsets that
      on the fly.

--- R6/Pseudobulk.R
+++ R6/Pseudobulk.R
@@ -5,10 +5,8 @@ here::i_am("R6/Pseudobulk.R")
 
 source(here("R6/CountMatrix.R"))
 
-
 Pseudobulk <- R6Class(
   "Pseudobulk",
-  inherit = CountMatrix,
   public = list(
     params = list(
       id = NULL,
@@ -20,25 +18,38 @@ Pseudobulk <- R6Class(
         scale_factor = NULL
       )
     ),
-    initialize = function(experiment, cell_indices,
+    initialize = function(count_matrix, cell_indices,
                           split_cancer,
-                          cancer_celltypes,
-                          normalization_type = "lognorm",
-                          scale_factor = 1) {
-      # TODO Check experiment type.
-      # TODO Check cexperimetn matrix and meta ordering.
-      self$matrix <- experiment$matrix[, cell_indices]
-      self$meta <- experiment$meta %>%
-        filter(cell %in% self$cells)
+                          cancer_celltypes) {
+      stopifnot(
+        all(c("CountMatrix", "R6") %in% class(count_matrix))
+      )
+      private$count_matrix <- count_matrix
+      private$cell_indices <- cell_indices
 
       self$params$split_cancer <- split_cancer
       self$params$cancer_celltypes <- cancer_celltypes
-
-      self$params$normalization$type <- normalization_type
-      self$params$normalization$scale_factor <- scale_factor
     }
   ),
   active = list(
+    matrix_raw = function() {
+      private$count_marix$matrix
+    },
+    matrix = function() {
+      self$matrix_raw %>%
+        extract(
+          i = ,
+          j = private$cell_indices,
+          drop = FALSE
+        )
+    },
+    meta_raw = function() {
+      private$count_matrix$meta
+    },
+    meta = function() {
+      self$meta_raw %>%
+        filter(cell %in% colnames(self$count_matrix))
+    },
     transcript_counts = function() {
       self$matrix %>%
         rowSums() %>%
@@ -48,7 +59,7 @@ Pseudobulk <- R6Class(
         )
     },
     transcript_counts_cancer = function() {
-      cancer_idcs <- count_matrix$celltypes %in% self$params$cancer_celltypes
+      cancer_idcs <- self$meta$celltypes %in% self$params$cancer_celltypes
 
       self$matrix[, cancer_idcs] %>%
         rowSums() %>%
@@ -58,12 +69,16 @@ Pseudobulk <- R6Class(
         )
     },
     celltype_counts = function() {
-      self$celltypes %>%
+      self$meta$celltypes %>%
         table() %>%
         {
           # Conversion from table to named vector.
           set_names(as.vector(.), names(.))
         }
     }
+  ),
+  private = list(
+    count_matrix = NULL,
+    cell_indices = NULL
   )
 )
