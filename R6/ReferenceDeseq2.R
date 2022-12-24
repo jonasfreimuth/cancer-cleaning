16d1ec430c749cd060433d1125ef333c48ae449d
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-24 11:29:25 +0100

    Add preliminary ReferenceDeseq2 (sub)class

---
+++ R6/ReferenceDeseq2.R
@@ -0,0 +1,16 @@
+library("here")
+library("R6")
+
+here::i_am("R6/ReferenceDeseq2.R")
+
+source(here("R6/Reference.R"))
+
+ReferenceDeseq2 <- R6Class(
+  "ReferenceDeseq2",
+  inherit = Reference,
+  public = list(
+    initialize = function(count_mat, threshold, markers) {
+      super$initialize(count_mat, threshold, markers)
+    }
+  )
+)
