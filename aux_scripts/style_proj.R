ba14796893c345104be9ab64f51fed7d39444cea
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-24 12:23:07 +0100

    Add script for styling project R files

---
+++ aux_scripts/style_proj.R
@@ -0,0 +1,11 @@
+library("styler")
+
+list_dir <- function(path) {
+  dir(path, full.names = TRUE, pattern = "*.R")
+}
+
+style_file(c(
+  list_dir("functions"),
+  list_dir("scripts"),
+  list_dir("aux_scripts")
+))
