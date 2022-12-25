afd07fb2a9c4e2f760a460195655fa25b39d9009
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-12-25 19:17:39 +0000

    Add preliminary class DeconvolutionSummary
      Should summarize a list of Deconvolutions, accounting for different
      parameters.

---
+++ R6/DeconvolutionSummary.R
@@ -0,0 +1,36 @@
+library("here")
+library("R6")
+
+suppressMessages(
+  here::i_am("R6/DeconvolutionSummary.R")
+)
+
+DeconvolutionSummary <- R6Class(
+  "DeconvolutionSummary",
+  public = list(
+    initialize = function(deconvolution_list) {
+      private$.check_deconv_list(deconvolution_list)
+      private$.deconvolution_list <- deconvolution_list
+    }
+  ),
+  active = list(
+    df = function() {
+      stop("Not implemented.")
+    }
+  ),
+  private = list(
+    .deconvolution_list = NULL,
+    .check_deconv_list = function(deconv_list) {
+      all_deconvolution <- lapply(
+        deconv_list,
+        function(deconv) {
+          all(c("Deconvolution", "R6") %in% class(deconv))
+        }
+      )
+
+      stopifnot(
+        all_deconvolution
+      )
+    }
+  )
+)
