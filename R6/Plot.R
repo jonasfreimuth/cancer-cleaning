479d6d64e89487c0a49405523510f0992bb651f8
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2023-01-06 16:51:03 +0100

    Plot: Add save method

--- R6/Plot.R
+++ R6/Plot.R
@@ -24,6 +24,20 @@ Plot <- R6Class(
       private$.check_params(param_list)
 
       private$.params <- param_list
+    },
+    save = function(dir, filename) {
+      path <- here(dir, filename)
+
+      self$plot %>%
+        {
+          ggsave(
+            plot = .,
+            filename = path,
+            width = self$plot_width,
+            height = self$plot_height,
+            limitsize = FALSE
+          )
+        }
     }
   ),
   active = list(
@@ -32,9 +46,24 @@ Plot <- R6Class(
     },
     themes = function() {
       private$.themes
+    },
+    plot_width = function() {
+      if (is.null(private$.plot_width)) {
+        private$.compute_plot_width()
+      }
+      private$.plot_width
+    },
+    plot_height = function() {
+      if (is.null(private$.plot_height)) {
+        private$.compute_plot_height()
+      }
+      private$.plot_height
     }
   ),
   private = list(
+    .plot = NULL,
+    .plot_width = NULL,
+    .plot_height = NULL,
     .params = NULL,
     .themes = list(
       theme_benchmark = function(...) {
@@ -45,6 +74,17 @@ Plot <- R6Class(
           )
       }
     ),
+    .compute_plot_width = function() {
+      stop(paste(
+        "No class implementation found, and no superclass implementation",
+        "exists for plot_width."
+      ))
+    },
+    .compute_plot_height = function() {stop(paste(
+        "No class implementation found, and no superclass implementation",
+        "exists for plot_height."
+      ))
+    },
     .check_params = function(param_list) {
       with(
         param_list,
